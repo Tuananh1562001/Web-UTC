4dbf7ebbede61d51ab5b954ee968ec38a32f6381
Tuananh15062001 <tuananh15062001@gmail.com>
2021-12-23 16:15:34 +0700

    delete

--- app.js/index.js
+++
@@ -1,9 +0,0 @@
-const btnScrollToTOp = document.getElementById("btnScrollToTop");
-
-btnScrollToTOp.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-});
